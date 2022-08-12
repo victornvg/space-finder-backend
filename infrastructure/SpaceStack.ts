3339eb2d29b639ec1d33856100a6d4aa461e7051
Victor Nguyen <dungnguyen@nvg.vn>
2022-08-12 11:21:55 +0700

    Inital commit

---
+++ infrastructure/SpaceStack.ts
@@ -0,0 +1,29 @@
+import { Stack, StackProps } from 'aws-cdk-lib'
+import { Construct } from 'constructs';
+import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda'
+import { join } from 'path'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
+import { GenericTable } from './GenericTable'
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs'
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam'
+
+export class SpaceStack extends Stack {
+    private api = new RestApi(this, 'SpaceApi')
+    private spaceTable = new GenericTable('SpaceTable', 'spaceId', this)
+    constructor(scope: Construct, id: string, props: StackProps) {
+        super(scope, id, props)
+
+        const helloLambdaNodeJs = new NodejsFunction(this, 'helloLambdaNodeJs', {
+            entry: join(__dirname, '..', 'services', 'node-lambda', 'hello.ts'),
+            handler: 'handler'
+        })
+        const s3ListPolicy = new PolicyStatement()
+        s3ListPolicy.addActions('s3:ListAllMyBuckets')
+        s3ListPolicy.addResources('*')
+        helloLambdaNodeJs.addToRolePolicy(s3ListPolicy)
+
+        const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs)
+        const helloLambdaResouce = this.api.root.addResource('hello')
+        helloLambdaResouce.addMethod('GET', helloLambdaIntegration)
+    }
+}
---
+++ services/hello/hello.js
@@ -0,0 +1,8 @@
+
+
+exports.main = async function (event, context) {
+    return {
+        statusCode: 200,
+        body: 'hello from lambda!'
+    }
+}
